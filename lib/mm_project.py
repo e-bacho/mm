954dcb42bbe6caf9e09963e46616221df022f60a
e-bacho <bacvovski@gmail.com>
2013-11-19 09:03:50 +1100

    Execute annonymous log fix for windows
    The file name format is not valid for Windows, please replace colon : with a dot or something else when running on Win

--- lib/mm_project.py
+++ lib/mm_project.py
@@ -1713,6 +1713,7 @@ class MavensMateProject(object):
     def __log_anonymous_apex(self, apex_body, log, script_name=None):
         if not os.path.exists(os.path.join(self.location, "apex-scripts", "log")):
             os.makedirs(os.path.join(self.location, "apex-scripts", "log"))
+        # The file name format is not valid for Windows, please replace colon : with a dot or something else when running on Win
         location = os.path.join(self.location, "apex-scripts", "log", mm_util.get_timestamp()+".json")
         src = open(location, "w")
         file_body = ""
